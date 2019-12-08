37e174e8ca66f99e30b317937c57b5f33dbb5102
Jillian Hobaica <jdh170@miami.edu>
2019-12-08 18:35:42 -0500

    Update script.js

--- homework/final/script.js
+++ homework/final/script.js
@@ -1,14 +1,1 @@
-var bioClicked = 0;
 
-$(document).ready(function() {
-$("#bio").mouseover(function() {
-console.log(bioClicked)
-if (bioClicked === 0) {
-$("#bio").css("font-size", "250px");
-bioClicked = 1;
-} else {
-$("#bio").css("font-size", "200px");
-bioClicked = 0;
-}
-});
-});
