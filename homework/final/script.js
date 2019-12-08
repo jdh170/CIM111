d3b2433e63e881f52869913d5aebd3b7f2aab517
Jillian Hobaica <jdh170@miami.edu>
2019-12-08 17:55:33 -0500

    final proj edits round 4

--- homework/final/script.js
+++ homework/final/script.js
@@ -1,14 +1,14 @@
+var bioClicked = 0;
 
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+$(document).ready(function() {
+$("#bio").mouseover(function() {
+console.log(bioClicked)
+if (bioClicked === 0) {
+$("#bio").css("font-size", "250px");
+bioClicked = 1;
+} else {
+$("#bio").css("font-size", "200px");
+bioClicked = 0;
 }
+});
+});
