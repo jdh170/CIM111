9f9f704cd3694cc4cfe9b2549f87623085eb33b2
Jillian Hobaica <jdh170@miami.edu>
2019-12-09 00:24:02 -0500

    added padding and javascript

--- homework/final/script.js
+++ homework/final/script.js
@@ -1,1 +1,14 @@
+var centeredClicked = 0;
 
+$(document).ready(function() {
+$("#centered").mouseover(function() {
+console.log(centeredClicked)
+if (centeredClicked === 0) {
+$("#centered").css("font-size", "150px");
+centeredClicked = 1;
+} else {
+$("#centered").css("font-size", "200px");
+centeredClicked = 0;
+}
+});
+});
