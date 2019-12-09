cb343ab03381f285e4ee5eda460e8874daa5b7e3
Jillian Hobaica <jdh170@miami.edu>
2019-12-09 00:36:02 -0500

    new fonts

--- homework/final/script.js
+++ homework/final/script.js
@@ -1,14 +1,14 @@
-var centeredClicked = 0;
+var jillianClicked = 0;
 
 $(document).ready(function() {
-$("#centered").mouseover(function() {
-console.log(centeredClicked)
-if (centeredClicked === 0) {
-$("#centered").css("font-size", "150px");
-centeredClicked = 1;
+$("#jillian").mouseover(function() {
+console.log(jillianClicked)
+if (jillianClicked === 0) {
+$("#jillian").css("font-size", "150px");
+jillianClicked = 1;
 } else {
-$("#centered").css("font-size", "200px");
-centeredClicked = 0;
+$("#jillian").css("font-size", "200px");
+jillianClicked = 0;
 }
 });
 });
